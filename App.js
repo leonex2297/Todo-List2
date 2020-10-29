5d2a1abf126f3bb63952c67e0ba5c3371fea2e96
Rahul <leonex2297@gmail.com>
2020-10-29 09:42:56 +0530

    main js files

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./index.css";
+import Todolist from "./Todolist";
+const App = () => {
+    const [list, setList] = useState();
+    const [item, setItem] = useState([]);
+    const inputEvent = (val) => {
+        setList(val.target.value)
+
+    }
+
+    const submitsout = () => {
+        console.log("submit");
+        setItem((listval) => {
+            return ([...listval, list]);
+        });
+        setList("");
+    }
+
+    return (
+        <React.Fragment>
+            <div className="container">
+                <br />
+                <h1 className="h1">ToDo List</h1>
+                <br />
+                <input className="input" type="text" name="list" placeholder="Item Name" onChange={inputEvent} value={list} />
+                <button className="btn" type="button" onClick={submitsout} >+</button>
+                <ol>
+                    {item.map((ar, index) => {
+                        return (<Todolist
+                            key={index}
+                            id={index}
+                            text={ar}
+                        />);
+                    })}
+                </ol>
+
+            </div>
+
+        </React.Fragment>
+    );
+}
+export default App;
